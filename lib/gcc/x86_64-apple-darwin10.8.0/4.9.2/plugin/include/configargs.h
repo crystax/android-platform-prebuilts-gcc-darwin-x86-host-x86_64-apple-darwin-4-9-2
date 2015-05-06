de1335969ee514021f75402c5cee4d48989c2006
Alexander Zhukov <zuav@crystax.net>
2015-05-06 14:16:09 +0300

    Inital commit

---
+++ lib/gcc/x86_64-apple-darwin10.8.0/4.9.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/tmp/gcc/gcc-4.9.2/configure --prefix=/tmp/gcc/x86_64-apple-darwin-4.9.2 --enable-checking=release --with-gmp=/tmp/gcc/libs --with-mpfr=/tmp/gcc/libs --with-mpc=/tmp/gcc/libs --with-isl=/tmp/gcc/libs --with-cloog=/tmp/gcc/libs --enable-languages=c,c++ --with-pkgversion='Crystax NDK 10'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "core2" } };
